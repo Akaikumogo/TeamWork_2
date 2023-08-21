504334694d0f1a0a51c49c555171965f5d740ea1
Akaikumogo <98486553+Akaikumogo@users.noreply.github.com>
2023-08-21 14:11:37 +0500

    main

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Client from "./components/Client/Client";
+import Customer from "./components/Customer/Customer";
+import Footer from "./components/Footer/Footer";
+function App() {
+  return <></>;
+}
+
+export default App;
