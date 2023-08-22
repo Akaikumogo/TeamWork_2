bb6ce17c0f12e1eae326371747be20f49992e3e1
Akaikumogo <sarvarbekred147@gmail.com>
2023-08-22 18:05:13 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-//import Header from "./components/Header/Header";
-//import Client from "./components/Client/Client";
+import Header from "./components/Header/Header";
+import Client from "./components/Client/Client";
 import Customer from "./components/Customer/Customer";
-//import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
 function App() {
   return <>
   <Customer />
