0b4e2f57531bc8368936978473dc1d66d74bef21
Akaikumogo <sarvarbekred147@gmail.com>
2023-08-22 18:42:44 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Client from "./components/Client/Client";
 import Customer from "./components/Customer/Customer";
-import Footer from "./components/Footer/Footer";
+//import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
       <Header />
+      <Client />
+      <Customer />
     </>
   );
+}
 export default App;
