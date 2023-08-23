bdf59ed58d6826d4554a1189859c51037357b4a2
Akaikumogo <sarvarbekred147@gmail.com>
2023-08-23 07:38:55 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Header from "./components/Header/Header";
 import Client from "./components/Client/Client";
 import Customer from "./components/Customer/Customer";
 import Footer from "./components/Footer/Footer";
+import Works from "./components/Works/Works";
 function App() {
   return (
     <>
       <Header />
       <Customer />
+      <Works />
       <Client />
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -1,1 +1,31 @@
-
+import React from "react";
+import create from "../../images/work-1.svg";
+import shield from "../../images/shield.svg";
+import emoji from "../../images/emoji.svg";
+function works() {
+  return (
+    <div className="body">
+      <div className="container1">
+        <h2 className="work_title1">How it works</h2>
+        <div className="card_wrapper1">
+          <div className="card1">
+            <img className="img1" src={create} alt="peopleAdd" />
+            <h3 className="card_title1">Create an account</h3>
+            <p className="card_desc1">Lorem ipsum dolor sit amet,<br />consecteteu.</p>
+          </div>
+          <div className="card1">
+            <img className="img1" src={shield} alt="peopleAdd" />
+            <h3 className="card_title1">Get authorization</h3>
+            <p className="card_desc1">Lorem ipsum dolor sit  amet,<br />consecteteu.</p>
+          </div>
+          <div className="card1">
+            <img className="img1" src={emoji} alt="peopleAdd" />
+            <h3 className="card_title1">Enjoy the app</h3>
+            <p className="card_desc1">Lorem ipsum dolor sit amet,<br />consecteteu.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default works;
